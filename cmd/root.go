f4dd5a98f2ae08f5853f0ea32dd670a6dace5a55
raha <elahe.dstn@gmail.com>
2020-07-23 13:34:33 +0430

    Remove lint issues

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,7 @@ import (
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
+//nolint: gofumpt
 func Execute() {
 	// rootCmd represents the base command when called without any subcommands
 	var rootCmd = &cobra.Command{
