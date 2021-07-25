f5b1464f83ceb734cffca7eec7bd4029cfb6baa5
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 00:47:17 +0430

    feat: Add Healthz Handler

---
+++ internal/http/handler/healthz_test.go
@@ -0,0 +1,44 @@
+package handler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/1995parham/fandogh/internal/http/handler"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/suite"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+type HealthzSuite struct {
+	suite.Suite
+
+	engine *echo.Echo
+}
+
+func (suite *HealthzSuite) SetupSuite() {
+	suite.engine = echo.New()
+
+	handler.Healthz{
+		Logger: zap.NewNop(),
+		Tracer: trace.NewNoopTracerProvider().Tracer(""),
+	}.Register(suite.engine.Group(""))
+}
+
+func (suite *HealthzSuite) TestHandler() {
+	require := suite.Require()
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/healthz", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	suite.engine.ServeHTTP(w, req)
+	require.Equal(http.StatusNoContent, w.Code)
+}
+
+func TestHealthzSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(HealthzSuite))
+}
