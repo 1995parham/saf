009234bd101843336940e98cb26d498f451796f5
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 01:28:51 +0430

    fix: Correct Event Request

--- internal/http/request/event_test.go
+++ internal/http/request/event_test.go
@@ -3,133 +3,41 @@ package request_test
 import (
 	"testing"
 
-	"github.com/1995parham/fandogh/internal/http/request"
+	"github.com/1995parham/saf/internal/http/request"
 )
 
-// nolint: funlen
-func TestRegisterValidation(t *testing.T) {
+func TestEventValidation(t *testing.T) {
 	t.Parallel()
 
 	cases := []struct {
-		name     string
-		password string
-		email    string
-		isValid  bool
+		request request.Event
+		isValid bool
 	}{
 		{
+			request: request.Event{
+				Subject: "",
+				Payload: []byte{},
+			},
 			isValid: false,
 		},
 		{
-			name:     "Parham Alvani",
-			password: "1234567",
-			email:    "parham.alvani@gmail.com",
-			isValid:  true,
+			request: request.Event{
+				Subject: "hello",
+				Payload: []byte{},
+			},
+			isValid: true,
 		},
 		{
-			name:     "Parham Alvani",
-			password: "1234567",
-			email:    "parham.alvani_gmail",
-			isValid:  false,
-		},
-		{
-			name:     "Parham Alvani",
-			password: "1234567",
-			email:    "parham.alvani@gmail",
-			isValid:  false,
-		},
-		{
-			name:     "Parham Alvani",
-			password: "1234567",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  true,
-		},
-		{
-			name:     "Parham Alvani",
-			password: "12345",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  false,
-		},
-		{
-			name:     "Parham Alvani",
-			password: "123456",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  true,
-		},
-		{
-			name:     "پرهام الوانی",
-			password: "123456",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  true,
+			request: request.Event{
+				Subject: "hello",
+				Payload: []byte("Hello World"),
+			},
+			isValid: true,
 		},
 	}
 
 	for _, c := range cases {
-		rq := request.Register{
-			Name:     c.name,
-			Email:    c.email,
-			Password: c.password,
-		}
-
-		err := rq.Validate()
-
-		if c.isValid && err != nil {
-			t.Fatalf("valid request %+v has error %s", rq, err)
-		}
-
-		if !c.isValid && err == nil {
-			t.Fatalf("invalid request %+v has no error", rq)
-		}
-	}
-}
-
-func TestLoginValidation(t *testing.T) {
-	t.Parallel()
-
-	cases := []struct {
-		password string
-		email    string
-		isValid  bool
-	}{
-		{
-			isValid: false,
-		},
-		{
-			password: "1234567",
-			email:    "parham.alvani@gmail.com",
-			isValid:  true,
-		},
-		{
-			password: "1234567",
-			email:    "parham.alvani_gmail",
-			isValid:  false,
-		},
-		{
-			password: "1234567",
-			email:    "parham.alvani@gmail",
-			isValid:  false,
-		},
-		{
-			password: "1234567",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  true,
-		},
-		{
-			password: "12345",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  false,
-		},
-		{
-			password: "123456",
-			email:    "parham.alvani@aut.ac.ir",
-			isValid:  true,
-		},
-	}
-
-	for _, c := range cases {
-		rq := request.Login{
-			Email:    c.email,
-			Password: c.password,
-		}
+		rq := c.request
 
 		err := rq.Validate()
 
