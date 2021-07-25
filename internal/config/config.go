76e75d43f8aeac05193901728f108fd1e71b9388
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 00:08:00 +0430

    feat: Add Configuration

---
+++ internal/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"log"
+	"strings"
+
+	"github.com/1995parham/saf/internal/logger"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
+const (
+	// Prefix indicates environment variables prefix.
+	Prefix = "saf_"
+)
+
+type (
+	// Config holds all configurations.
+	Config struct {
+		Logger logger.Config `koanf:"logger"`
+	}
+)
+
+// New reads configuration with viper.
+func New() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default configuration from file
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading default: %s", err)
+	}
+
+	// load configuration from file
+	if err := k.Load(file.Provider("config.yml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s", err)
+	}
+
+	// load environment variables
+	if err := k.Load(env.Provider(Prefix, ".", func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(
+			strings.TrimPrefix(s, Prefix)), "_", ".")
+	}), nil); err != nil {
+		log.Printf("error loading environment variables: %s", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s", err)
+	}
+
+	log.Printf("following configuration is loaded:\n%+v", instance)
+
+	return instance
+}
