052209798bc5d8848d8a95c576056e93bca09f5a
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 01:14:47 +0430

    feat: Add Producer Command

--- go.mod
+++ go.mod
@@ -9,7 +9,8 @@ require (
 	github.com/cpuguy83/go-md2man v1.0.10 // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/knadh/koanf v1.2.0
-	github.com/labstack/echo/v4 v4.4.0 // indirect
+	github.com/labstack/echo/v4 v4.4.0
+	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/common v0.29.0 // indirect
 	github.com/prometheus/procfs v0.7.1 // indirect
@@ -17,6 +18,7 @@ require (
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/tchap/zapext/v2 v2.1.0
 	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.21.0
 	go.opentelemetry.io/otel v1.0.0-RC1
 	go.opentelemetry.io/otel/exporters/jaeger v1.0.0-RC1
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.0.0-RC1
@@ -25,6 +27,8 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 	go.uber.org/zap v1.18.1
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210716203947-853a461950ff // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
---
+++ internal/cmd/producer/main.go
@@ -0,0 +1,53 @@
+package producer
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/1995parham/saf/internal/config"
+	"github.com/1995parham/saf/internal/http/handler"
+	"github.com/1995parham/saf/internal/metric"
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+func main(cfg config.Config, logger *zap.Logger, tracer trace.Tracer) {
+	metric.NewServer(cfg.Monitoring).Start(logger.Named("metrics"))
+
+	app := echo.New()
+
+	app.Use(otelecho.Middleware("saf"))
+
+	handler.Healthz{
+		Logger: logger.Named("handler").Named("healthz"),
+		Tracer: tracer,
+	}.Register(app.Group(""))
+
+	if err := app.Start(":1378"); !errors.Is(err, http.ErrServerClosed) {
+		logger.Fatal("echo initiation failed", zap.Error(err))
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+}
+
+// Register server command.
+func Register(root *cobra.Command, cfg config.Config, logger *zap.Logger, tracer trace.Tracer) {
+	root.AddCommand(
+		// nolint: exhaustivestruct
+		&cobra.Command{
+			Use:   "producer",
+			Short: "gets events from http and produce them into nats",
+			Run: func(cmd *cobra.Command, args []string) {
+				main(cfg, logger, tracer)
+			},
+		},
+	)
+}
