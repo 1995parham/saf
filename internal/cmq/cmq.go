51feb06f49d187f2db01354a45bed9c40d82da4d
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 01:54:32 +0430

    feat: Add Stream Creation

--- internal/cmq/cmq.go
+++ internal/cmq/cmq.go
@@ -1,13 +1,21 @@
 package cmq
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
 	"github.com/nats-io/nats.go"
 	"go.uber.org/zap"
 )
 
-func New(cfg Config, logger *zap.Logger) (*nats.Conn, error) {
+type CMQ struct {
+	Conn   *nats.Conn
+	JConn  nats.JetStreamContext
+	Logger *zap.Logger
+}
+
+func New(cfg Config, logger *zap.Logger) (*CMQ, error) {
 	nc, err := nats.Connect(cfg.URL)
 	if err != nil {
 		return nil, fmt.Errorf("nats connection failed %w", err)
@@ -25,5 +33,40 @@ func New(cfg Config, logger *zap.Logger) (*nats.Conn, error) {
 		logger.Warn("nats reconnected")
 	})
 
-	return nc, nil
+	jsm, err := nc.JetStream()
+	if err != nil {
+		return nil, fmt.Errorf("jetstream context extraction failed %w", err)
+	}
+
+	return &CMQ{
+		Conn:   nc,
+		JConn:  jsm,
+		Logger: logger,
+	}, nil
+}
+
+func (c *CMQ) Streams() error {
+	info, err := c.JConn.StreamInfo("ORDERS")
+
+	switch {
+	case errors.Is(err, nats.ErrStreamNotFound):
+		// nolint: exhaustivestruct
+		stream, err := c.JConn.AddStream(&nats.StreamConfig{
+			Name:     "events",
+			Subjects: []string{"events.>"},
+			MaxAge:   1 * time.Hour,
+			Storage:  nats.MemoryStorage,
+		})
+		if err != nil {
+			return fmt.Errorf("cannot create stream %w", err)
+		}
+
+		info = stream
+	case err != nil:
+		return fmt.Errorf("cannot read stream information %w", err)
+	}
+
+	c.Logger.Info("events stream", zap.Any("stream", info))
+
+	return nil
 }
