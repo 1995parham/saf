5730345976b472e604a74f4fa4c43655a0ba8252
Parham Alvani <parham.alvani@gmail.com>
2021-07-26 00:47:17 +0430

    feat: Add Healthz Handler

---
+++ internal/http/handler/healthz.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+type Healthz struct {
+	Logger *zap.Logger
+	Tracer trace.Tracer
+}
+
+// Handle shows server is up and running.
+// nolint: wrapcheck
+func (h Healthz) Handle(c echo.Context) error {
+	_, span := h.Tracer.Start(c.Request().Context(), "handler.healthz")
+	defer span.End()
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+// Register registers the routes of healthz handler on given echo group.
+func (h Healthz) Register(g *echo.Group) {
+	g.GET("/healthz", h.Handle)
+}
