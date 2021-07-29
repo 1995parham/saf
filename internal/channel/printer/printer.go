563cfb240d76b79e99515e518eb6346a8fad7553
Parham Alvani <parham.alvani@gmail.com>
2021-07-29 10:06:55 +0430

    feat: Add MQTT Channel and Switch to Channeled Event

--- internal/channel/printer/printer.go
+++ internal/channel/printer/printer.go
@@ -4,30 +4,35 @@ import (
 	"runtime"
 
 	"github.com/1995parham/saf/internal/model"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 // Printer is a plugin for the saf app. this plugin consumes event
 // and log them.
 type Printer struct {
-	ch     <-chan model.Event
+	ch     <-chan model.ChanneledEvent
 	logger *zap.Logger
+	tracer trace.Tracer
 }
 
-func (p *Printer) Init(logger *zap.Logger, cfg interface{}) {
+func (p *Printer) Init(logger *zap.Logger, tracer trace.Tracer, cfg interface{}) {
 	p.logger = logger
+	p.tracer = tracer
 }
 
 func (p *Printer) Run() {
 	for i := 0; i < 10*runtime.GOMAXPROCS(0); i++ {
 		go func() {
 			for e := range p.ch {
-				e.Span.SetName("channels.printer")
+				_, span := p.tracer.Start(e.Context, "channels.printer")
+
 				p.logger.Info("receive event",
 					zap.Time("created", e.CreatedAt),
 					zap.String("subject", e.Subject),
 				)
-				e.Span.End()
+
+				span.End()
 			}
 		}()
 	}
@@ -37,6 +42,6 @@ func (p *Printer) Name() string {
 	return "printer"
 }
 
-func (p *Printer) SetChannel(c <-chan model.Event) {
+func (p *Printer) SetChannel(c <-chan model.ChanneledEvent) {
 	p.ch = c
 }
