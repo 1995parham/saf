50f83690a9e4f052554e9a0c01b8539289a66db5
Parham Alvani <parham.alvani@gmail.com>
2021-07-29 17:36:44 +0430

    feat: Add Component Name to Profiler

--- internal/telemetry/profiler/profiler.go
+++ internal/telemetry/profiler/profiler.go
@@ -1,17 +1,18 @@
 package profiler
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/1995parham/saf/internal/telemetry/config"
 	"github.com/pyroscope-io/pyroscope/pkg/agent/profiler"
 )
 
-func Start(cfg config.Profiler) {
+func Start(cfg config.Profiler, component string) {
 	if cfg.Enabled {
 		// nolint: exhaustivestruct
 		if _, err := profiler.Start(profiler.Config{
-			ApplicationName: "1995parham/saf",
+			ApplicationName: fmt.Sprintf("1995parham.saf.%s", component),
 			ServerAddress:   cfg.Address,
 		}); err != nil {
 			log.Printf("failed to start the profiler")
