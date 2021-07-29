dedb7c1743fcd87cb8b10aa1dd5738d5bd47a333
Parham Alvani <parham.alvani@gmail.com>
2021-07-29 10:06:55 +0430

    feat: Add MQTT Channel and Switch to Channeled Event

--- internal/channel/channel.go
+++ internal/channel/channel.go
@@ -1,15 +1,17 @@
 package channel
 
 import (
+	"github.com/1995parham/saf/internal/channel/mqtt"
 	"github.com/1995parham/saf/internal/channel/printer"
 	"github.com/1995parham/saf/internal/model"
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 )
 
 type Channel interface {
-	Init(*zap.Logger, interface{})
+	Init(*zap.Logger, trace.Tracer, interface{})
 	Run()
-	SetChannel(<-chan model.Event)
+	SetChannel(<-chan model.ChanneledEvent)
 	Name() string
 }
 
@@ -17,17 +19,19 @@ type Channel interface {
 type Manager struct {
 	Plugins []Channel
 	logger  *zap.Logger
+	tracer  trace.Tracer
 
-	channels []chan model.Event
+	channels []chan model.ChanneledEvent
 }
 
 // New create new manager.
-func New(logger *zap.Logger) *Manager {
+func New(logger *zap.Logger, tracer trace.Tracer) *Manager {
 	return &Manager{
 		Plugins: make([]Channel, 0),
 		logger:  logger,
+		tracer:  tracer,
 
-		channels: make([]chan model.Event, 0),
+		channels: make([]chan model.ChanneledEvent, 0),
 	}
 }
 
@@ -36,6 +40,7 @@ func (m *Manager) Setup(enabled []string, cfg map[string]interface{}) {
 	// list of available channles, please add each channel into this list to make them available.
 	channels := []Channel{
 		&printer.Printer{},
+		&mqtt.MQTT{},
 	}
 
 	for _, p := range channels {
@@ -54,19 +59,19 @@ func (m *Manager) Register(p Channel, cfg interface{}) {
 
 	m.Plugins = append(m.Plugins, p)
 
-	c := make(chan model.Event)
+	c := make(chan model.ChanneledEvent)
 
 	m.channels = append(m.channels, c)
 
-	p.Init(m.logger.Named(p.Name()), cfg)
+	p.Init(m.logger.Named(p.Name()), m.tracer, cfg)
 	p.SetChannel(c)
 
 	go p.Run()
 }
 
 // Channels return the channels to register them on subscriber in the main.
-func (m *Manager) Channels() []chan<- model.Event {
-	c := make([]chan<- model.Event, len(m.channels))
+func (m *Manager) Channels() []chan<- model.ChanneledEvent {
+	c := make([]chan<- model.ChanneledEvent, len(m.channels))
 	for i := range m.channels {
 		c[i] = m.channels[i]
 	}
