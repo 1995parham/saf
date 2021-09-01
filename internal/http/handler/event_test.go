f5c296df2d1dac61676bd42d0f8f03a6eb4230f1
Parham Alvani <1995parham@tuta.io>
2021-09-01 06:43:15 +0430

    feat: add touchy test for event handler

---
+++ internal/http/handler/event_test.go
@@ -0,0 +1,62 @@
+package handler_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/1995parham/saf/internal/cmq"
+	"github.com/1995parham/saf/internal/config"
+	"github.com/1995parham/saf/internal/http/handler"
+	"github.com/1995parham/saf/internal/http/request"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/suite"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+)
+
+type EventSuite struct {
+	suite.Suite
+
+	engine *fiber.App
+}
+
+func (suite *EventSuite) SetupSuite() {
+	suite.engine = fiber.New()
+
+	cfg := config.New()
+
+	cmq, err := cmq.New(cfg.NATS, zap.NewNop())
+	suite.Require().NoError(err)
+
+	handler.Event{
+		CMQ:    cmq,
+		Logger: zap.NewNop(),
+		Tracer: trace.NewNoopTracerProvider().Tracer(""),
+	}.Register(suite.engine.Group(""))
+}
+
+func (suite *EventSuite) TestHandler() {
+	require := suite.Require()
+
+	payload, err := json.Marshal(request.Event{
+		Subject: "hello",
+		Payload: []byte("from the otherside"),
+	})
+	require.NoError(err)
+
+	req := httptest.NewRequest("POST", "/event", bytes.NewBuffer(payload))
+	req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
+
+	resp, err := suite.engine.Test(req)
+	require.NoError(err)
+	require.Equal(http.StatusNoContent, resp.StatusCode)
+	require.NoError(resp.Body.Close())
+}
+
+func TestEventSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(EventSuite))
+}
