039018fe276df3cf6c404d2d3e7e89cbfbfc6175
Parham Alvani <1995parham@tuta.io>
2021-09-07 20:01:10 +0430

    feat: add k6 for load testing

---
+++ k6/script.js
@@ -0,0 +1,31 @@
+import http from "k6/http";
+import { check, group } from "k6";
+
+const baseURL = "http://127.0.0.1:1378";
+
+export default function () {
+  group("healthz", () => {
+    let res = http.get(`${baseURL}/healthz`);
+
+    check(res, {
+      success: (res) => res.status === 204,
+    });
+  });
+  group("event", () => {
+    group("send", () => {
+      let payload = JSON.stringify({
+        subject: "elahe",
+      });
+
+      let res = http.post(`${baseURL}/api/event`, payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      check(res, {
+        success: (res) => res.status == 200,
+      });
+    });
+  });
+}
