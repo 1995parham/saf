952fff2d8731350fb404aa6ec8879a9258c30faa
Parham Alvani <parham.alvani@gmail.com>
2021-09-22 13:04:46 +0330

    feat: add automaxprocs

--- cmd/saf/main.go
+++ cmd/saf/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/1995parham/saf/internal/cmd"
+import (
+	"github.com/1995parham/saf/internal/cmd"
+	_ "go.uber.org/automaxprocs"
+)
 
 func main() {
 	cmd.Execute()
