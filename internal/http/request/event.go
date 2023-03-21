dd9baf8030a8f2e51689e7a8d047c89bfae967d1
Parham Alvani <parham.alvani@gmail.com>
2023-03-21 22:55:42 +0330

    feat: better validation

--- internal/http/request/event.go
+++ internal/http/request/event.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 const (
@@ -20,7 +21,7 @@ type Event struct {
 // Validate event request payload.
 func (r Event) Validate() error {
 	if err := validation.ValidateStruct(&r,
-		validation.Field(&r.Subject, validation.Required),
+		validation.Field(&r.Subject, is.Alphanumeric, validation.Required),
 	); err != nil {
 		return fmt.Errorf("event request validation failed: %w", err)
 	}
