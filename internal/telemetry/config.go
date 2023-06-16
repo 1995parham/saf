715a9dc10b2932090db22822b2beec567ccc1aad
Parham Alvani <parham.alvani@gmail.com>
2023-06-16 17:18:08 +0330

    feat: update telemetry package

---
+++ internal/telemetry/config.go
@@ -0,0 +1,24 @@
+package telemetry
+
+type Config struct {
+	Trace       Trace  `koanf:"trace"`
+	Meter       Meter  `koanf:"meter"`
+	Namespace   string `koanf:"namespace"`
+	ServiceName string `koanf:"service_name"`
+}
+
+type Meter struct {
+	Address string `koanf:"address"`
+	Enabled bool   `koanf:"enabled"`
+}
+
+type Trace struct {
+	Enabled bool    `koanf:"enabled"`
+	Ratio   float64 `koanf:"ratio"`
+	Agent   Agent   `koanf:"agent"`
+}
+
+type Agent struct {
+	Host string `koanf:"host"`
+	Port string `koanf:"port"`
+}
