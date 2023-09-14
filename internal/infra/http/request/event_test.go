60923875c6f35450fcd5292449c5f03b81a6e998
Parham Alvani <parham.alvani@gmail.com>
2023-09-14 07:07:43 +0000

    feat: complete http request validation with tests

---
+++ internal/infra/http/request/event_test.go
@@ -0,0 +1,76 @@
+package request_test
+
+import (
+	"testing"
+
+	"github.com/1995parham/saf/internal/infra/http/request"
+)
+
+func TestEventValidation(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		request request.Event
+		isValid bool
+	}{
+		{
+			request: request.Event{
+				Subject: "",
+				ID:      "",
+				Service: "",
+				Payload: []byte{},
+			},
+			isValid: false,
+		},
+		{
+			request: request.Event{
+				Subject: "hello",
+				ID:      "",
+				Service: "OfferService",
+				Payload: []byte{},
+			},
+			isValid: true,
+		},
+		{
+			request: request.Event{
+				Subject: "hello",
+				ID:      "",
+				Service: "",
+				Payload: []byte{},
+			},
+			isValid: false,
+		},
+		{
+			request: request.Event{
+				Subject: "hello",
+				ID:      "",
+				Service: "NewService",
+				Payload: []byte{},
+			},
+			isValid: false,
+		},
+		{
+			request: request.Event{
+				Subject: "hello",
+				ID:      "",
+				Payload: []byte("Hello World"),
+				Service: "RideLifeCycleService",
+			},
+			isValid: true,
+		},
+	}
+
+	for _, c := range cases {
+		rq := c.request
+
+		err := rq.Validate()
+
+		if c.isValid && err != nil {
+			t.Fatalf("valid request %+v has error %s", rq, err)
+		}
+
+		if !c.isValid && err == nil {
+			t.Fatalf("invalid request %+v has no error", rq)
+		}
+	}
+}
