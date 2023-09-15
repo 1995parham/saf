a22968c048bf2df339cb0f9cd2b53e52c14df92f
Parham Alvani <parham.alvani@gmail.com>
2023-09-15 14:08:50 +0000

    fix: remove global variable

--- internal/infra/output/manager/channel.go
+++ internal/infra/output/manager/channel.go
@@ -7,7 +7,9 @@ import (
 )
 
 // list of available channles, please add each channel into this list to make them available.
-var channels = []output.Channel{
-	new(printer.Printer),
-	new(mqtt.MQTT),
+func channels() []output.Channel {
+	return []output.Channel{
+		new(printer.Printer),
+		new(mqtt.MQTT),
+	}
 }
