986385e6464f0bab097248844dc7dbd68bf82cee
Parham Alvani <parham.alvani@gmail.com>
2025-11-24 12:53:59 +0000

    fix: use official context package

--- internal/infra/telemetry/provide.go
+++ internal/infra/telemetry/provide.go
@@ -1,6 +1,7 @@
 package telemetry
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -20,7 +21,6 @@ import (
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/fx"
-	"golang.org/x/net/context"
 )
 
 func setupTraceExporter(cfg Config) trace.SpanExporter {
